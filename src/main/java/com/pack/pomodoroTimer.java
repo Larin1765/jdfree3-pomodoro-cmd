76451fe91e8597745088fdb895b569fc9b1f55b3
Vik <viktor1765@yandex.ru>
2022-08-28 22:44:08 +0300

    code+

--- src/main/java/com/pack/pomodoroTimer.java
+++ src/main/java/com/pack/pomodoroTimer.java
@@ -1,7 +1,39 @@
 package com.pack;
 
+import java.util.Scanner;
+
 public class pomodoroTimer {
     public static void main(String[] args) {
-        System.out.println("HW");
+        System.out.println("Введите время работы и отдыха");
+        //[-w , 1 , -b , 1 ]
+       var cmd = new Scanner(System.in).nextLine().split("");
+       int workTime=1;
+       int breakTime=1;
+       boolean isHelp=false;
+       for (int i=0; i<cmd.length;i++){
+           switch (cmd[i]) {
+               case "-w" -> workTime = Integer.parseInt(cmd[++i]);
+               case "-b" -> breakTime = Integer.parseInt(cmd[++i]);
+               case "--help" -> {
+                   System.out.println("""
+                           \nPomodoro -это приложение для улучшения личной эффективности
+                           -w - сколько работать
+                           -b -сколько отдыхать
+                           --help -вызвать помощь
+                           """);
+                   isHelp = true;
+               }
+           }
+
+       }
+       if (isHelp) return;
+       long startTime=System.currentTimeMillis();
+       // time
+        long endTime=System.currentTimeMillis();
+        System.out.println("Таймер работал" +(endTime - startTime/1000*60)+"минут");
+
+    }
+    public static void timer(){
+
     }
 }
